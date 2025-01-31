cd50825d4c50fe86b016020cc6a79b1e3c1a8979
Soham Mitra <thesammit@gmail.com>
2025-01-31 21:12:30 +0530

    Error handling complete, end of chapter 14

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,20 +7,49 @@ import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.',
+  }),
+  amount: z.coerce
+    .number({
+      invalid_type_error: 'Please enter a Number.',
+    })
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please choose an invoice status.',
+  }),
   date: z.string(),
 });
+
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
+export async function createInvoice(prevState: State, formData: FormData) {
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+
+  // Prepare data for insertion into the database
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
@@ -31,22 +60,33 @@ export async function createInvoice(formData: FormData) {
     `;
   } catch (err) {
     console.error(err);
-    throw new Error('Failed to Create Invoice');
+    return {
+      message: 'Database Error: Failed to Create Invoice.',
+    };
   }
 
-
+  // Revalidate the cache for the invoices page and redirect the user.
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Update Invoice.',
+    };
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
-  // const date = new Date().toISOString().split('T')[0];
 
   try {
     await sql`
@@ -56,7 +96,9 @@ export async function updateInvoice(id: string, formData: FormData) {
     `;
   } catch (err) {
     console.error(err);
-    throw new Error('Failed to Update Invoice');
+    return {
+      message: 'Database Error: Failed to Update Invoice.',
+    };
   }
 
   revalidatePath('/dashboard/invoices');
@@ -68,7 +110,8 @@ export async function deleteInvoice(id: string) {
     await sql`
     DELETE FROM invoices WHERE id = ${id}
   `;
-  } catch (error) {
+  } catch (err) {
+    console.error(err);
     throw new Error('Failed to Delete Invoice');
   }
 
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -12,7 +12,7 @@ export default function NotFound() {
                     href="/dashboard"
                     className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
                 >
-                    Go to Dashboard
+                    Go to Home
                 </Link>
             </article>
         </main>
